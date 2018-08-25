dbf4ca94983cd34d129d80fd0cc51b7ce8a2dcaf
MairaCCB <League@AdministorsiMac.attlocal.net>
2018-08-25 16:03:48 -0700

    Mostly worked on the story and a few images

---
+++ src/Story.java
@@ -0,0 +1,20 @@
+/*
+ * CAUTION! You MUST read The Pen of Doom (El Bolígrafo de la Perdición), The
+ * Tale of Lava Face and Sausage Legs, Art-third and the Cheese Grater of
+ * Righteousness, and, of course, Hermetfleas. Hello, user. I am Narrator. I
+ * couldn’t care less about your name, so don’t waste your time. This story is
+ * about a man-fish kinda thingy. You’ll understand when you see him. Anyways,
+ * his name is Pierre. He always looked up to his hero Hermetfleas, the ugly guy
+ * who made Fleece important by killing Appleham Limburger. Pierre was a very
+ * passive - *screams at squirrel approaching* - check that, timid kind of a
+ * guy. One day, he heard from a man named Sausage Legs that Hermetfleas was in
+ * trouble. Their conversation went somewhat like this: “AAAAAAAA!” Pierre
+ * savagely said. ”AAAAAAAAAA!” Sausage Legs retorted in a truly magnificent
+ * fashion. This went on until the concept of Hermetfleas. In rough translation
+ * from scream to English, Larry betrayed Hermetfleas for something
+ * unidentifiable (Youtubers, put that in your Top Ten Anime Betrayals videos),
+ * and Hermetfleas was wallowing in his Fortress of Solidtoot in Toto, Africa.
+ * So, they resolved to find Larry and tell him Hermetfleas’ most famous quote,
+ * which would also serve as their battle cry: “HURRRRRR?”
+
+ */
